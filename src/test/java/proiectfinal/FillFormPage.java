721b91f4a2ebe8691cdc2871ec9768ed424a3398
gabianechiti <gabianechiti@gmail.com>
2024-12-09 18:12:56 +0200

    update

--- src/test/java/proiectfinal/FillFormPage.java
+++ src/test/java/proiectfinal/FillFormPage.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 public class FillFormPage extends BasePage{
 
-    @FindBy(xpath = "//button[contains(text(), 'Consent')]")
+    @FindBy (xpath = "/html/body/div/div[2]/div[2]/div[2]/div[2]/button[1]")
     private WebElement acceptCookies;
 
     @FindBy (css = "#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(4) > a > i")
